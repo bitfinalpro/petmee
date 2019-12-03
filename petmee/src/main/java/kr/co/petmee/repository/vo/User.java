e82464385f07f695de63b80d37d3bf6594c5bf02
KwonSungjin <boy9412@naver.com>
2019-12-03 17:16:26 +0900

    유저 VO

--- petmee/src/main/java/kr/co/petmee/repository/vo/User.java
+++ petmee/src/main/java/kr/co/petmee/repository/vo/User.java
@@ -1,5 +1,18 @@
 package kr.co.petmee.repository.vo;
 
-public class User {
+import lombok.Data;
 
+@Data
+public class User {
+   private String email;                    // 이메일
+   private String pass;                      // 비밀번호
+   private String name;                    // 이름
+   private String phone;                   // 핸드폰 번호
+   private String resident;               // 주민번호
+   private String Gender;                 //  성별
+   private String withdrawal;          // 탈퇴
+   private String usertype;              // 유저타입 ( 일반 / 관리자)
+   private String nickname;             // 닉네임 (쓸지말지)
+   private String address;               // 주소
+   private int age;                         // 나이
 }
