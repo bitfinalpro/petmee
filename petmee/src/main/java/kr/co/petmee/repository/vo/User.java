78410ed911d1d17736a9f7747f0d85c3ed0f87b2
shinchowon <scw3999@gmail.com>
2019-12-04 16:38:40 +0900

    유저vo수정

--- petmee/src/main/java/kr/co/petmee/repository/vo/User.java
+++ petmee/src/main/java/kr/co/petmee/repository/vo/User.java
@@ -3,22 +3,14 @@ package kr.co.petmee.repository.vo;
 import lombok.Data;
 
 @Data
-public class User {
-<<<<<<< HEAD
+public class User { 
    private String email;                    // 이메일
-=======
-   private int userNo;                       // 유저번호
-   private String email;                    // 이메일2
->>>>>>> freeBoard
+   private int userNo;                       // 유저번호                    // 이메일2
    private String pass;                      // 비밀번호
    private String name;                    // 이름
    private String phone;                   // 핸드폰 번호
    private String resident;               // 주민번호
-<<<<<<< HEAD
-   private String Gender;                 //  성별
-=======
    private String gender;                 //  성별
->>>>>>> freeBoard
    private String withdrawal;          // 탈퇴
    private String usertype;              // 유저타입 ( 일반 / 관리자)
    private String nickname;             // 닉네임 (쓸지말지)
