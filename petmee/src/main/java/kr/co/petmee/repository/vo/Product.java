03f81d4f7df7e6841dc6d654cf25a4b9f014c881
shinchowon <scw3999@gmail.com>
2019-12-08 20:14:17 +0900

    product 수정

--- petmee/src/main/java/kr/co/petmee/repository/vo/Product.java
+++ petmee/src/main/java/kr/co/petmee/repository/vo/Product.java
@@ -4,16 +4,6 @@ import lombok.Data;
 
 @Data
 public class Product {
-<<<<<<< HEAD
-	private String productId;
-	private String productName;
-	private String productInfo;
-	private int price;
-	private int productStock;
-	private String campany;
-	private int categoryNo;
-	private int animalNo;
-=======
 	private int categoryNo;
 	private String categoryName;
 	private int animalNo;
@@ -23,5 +13,4 @@ public class Product {
 	private int price;
 	private int stock;
 	private String company;	
->>>>>>> develop
 }
